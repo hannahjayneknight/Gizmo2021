1588e600912cc707d91ffd477cc830e49df21bef
Hannah Jayne Knight <hannah@tinpot.co.uk>
2021-01-29 15:11:23 +0000

    Task 3 attempted but not fully complete

--- Week2/DC_motor_enter_speed/DC_motor_enter_speed.ino
+++ Week2/DC_motor_enter_speed/DC_motor_enter_speed.ino
@@ -24,10 +24,12 @@ byte colPins[COLS] = {5, 4, 3, 2}; //connect to the column pinouts of the keypad
 Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
 
 /************************/
-
+int i = 0;                      // Variable to know the position in the sequence of a pressed number
 int int_customKey = 0;                // Variable to get the integer value of the pressed number
-int in_pass = 0;                      // The current speed being introduced
-int power[4] = {1000, 100, 10, 1};    // Array to get the result of 10^(2-i) when computing the integer value of the password being introduced
+int in_speed = 0;                      // The current speed being introduced
+int power[3] = {100, 10, 1};    // Array to get the result of 10^(2-i) when computing the integer value of the password being introduced
+
+
 /************************/
   
 void setup(){
@@ -36,40 +38,43 @@ void setup(){
   pinMode(DIRA,OUTPUT);
   pinMode(DIRB,OUTPUT);
   Serial.begin(9600);
+  Serial.println("Please type the fan speed as a 3 digit number (using leading zeroes where appropriate)"); 
 }
   
 void loop(){
   char customKey = customKeypad.getKey(); // the key that was pressed
-  analogWrite(ENABLE, int_pass); // sets initial speed of fan
-  digitalWrite(DIRA,HIGH);
-  digitalWrite(DIRB,LOW);
 
-  if (customKey){                               // when a key is pressed
+  if (customKey && (customKey != '#') ){        // when a key is pressed that is not the enter key
+    
     int_customKey = customKey - '0';            // Obtaining the integer value of the pressed button. ASCII code of the pressed button minus ASCII code of character '0'.
-    in_pass = in_pass + int_customKey*power[i]; // Computing the integer value of the password being introduced
-    Serial.print(in_pass);                      // Sending inputed key to the serial monitor
+    in_speed = in_speed + int_customKey*power[i]; // Computing the integer value of the password being introduced
+    i = i+1;
+    Serial.println(in_speed);
+  
   }
 
+  if (customKey == '#') {                       // when enter key is pressed
 
-
-  if (customKey == '#') {                 // # enters the speed of the fan
-
-    Serial.println();
-    
-    if ( 0 < isdigit(customKey)< 255) {   // checks if the entered phrase is an int between 0 and 255
-      
-      Serial.println("Fan speed has been set");
-      analogWrite(ENABLE, int_pass); // sets new speed of fan
-      digitalWrite(DIRA,HIGH);
+    if ( ((in_speed-255)*(in_speed-0) <= 0) ) {
+      Serial.println();
+      Serial.print("Fan speed has been set to ");
+      Serial.println(in_speed);
+      analogWrite(ENABLE, in_speed); //enable on
+      digitalWrite(DIRA,HIGH); //one way
       digitalWrite(DIRB,LOW);
-      in_pass = 0;                                // Resetting the value of the speed introduced
-      
+      delay(3000);
+      in_speed = 0 ;
+      i = 0;
+            
     }
     else {
-      
+      Serial.println();
       Serial.println("Please type in a speed between 0 and 255");
-      in_pass = 0;                                // Resetting the value of the speed introduced
-      
+      in_speed = 0;
+      i = 0;
     }
-    
+
+      
   }
+
+}
