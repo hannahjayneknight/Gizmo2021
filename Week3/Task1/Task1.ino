e05a52716051c622c8c04e6baaec57c58cb3dfb8
Hannah Jayne Knight <hannah@tinpot.co.uk>
2021-02-02 16:48:51 +0000

    Finished task1

--- Week3/Task1/Task1.ino
+++ Week3/Task1/Task1.ino
@@ -1,16 +1,9 @@
-// Sensor digital pin changed to pin 12 (instead of pin 3)
-// www.elegoo.com
-// 2018.10.24
-/*
-  LED1 should be lit, showing power. LED2 indicates sound input, and the sensitivity is adjusted by potentiometer.
-  There is a tiny screw on the blue potentiometer block that you can use for adjustment. Turning that
-  clockwise lowers the potentiometer value, while counter-clockwise raises the potentiometer value.
-  Use the potentiometer to adjust the Sound Sensor sensitivity. Turn the potentiometer
-  several rotations until you see the LED2 extinguish (or just faintly blink). This might be slightly greater than
-  500, if you are also watching Serial Monitor (inital adjustment), or, Serial Plotter (the latter is prefererd for observation).
-  Special thanks to user CRomer, for his input and hard work!
-*/
+// Lighting the RGB using sound.
+// Hannah Knight
+// 02/02/2021
 
+
+// For the sound sensor
 int  sensorAnalogPin = A0;    // Select the Arduino input pin to accept the Sound Sensor's analog output 
 int  sensorDigitalPin = 12;   // Select the Arduino input pin to accept the Sound Sensor's digital output
 int  analogValue = 0;         // Define variable to store the analog value coming from the Sound Sensor
@@ -19,23 +12,96 @@ int  Led13 = 13;              // Define LED port; this is the LED built in to th
                               // When D0 from the Sound Sensor (connnected to pin 12 on the
                               // Arduino) sends High (voltage present), L will light up. In practice, you
                               // should see LED13 on the Arduino blink when LED2 on the Sensor is 100% lit.
+
+// For the RGB
+#define RED 3
+#define GREEN 5
+#define BLUE 7
+int redValue;
+int greenValue;
+int blueValue;
+
                               
 
 void setup()
 {
+  // For the sound sensor
   Serial.begin(9600);               // The IDE settings for Serial Monitor/Plotter (preferred) must match this speed
   pinMode(sensorDigitalPin,INPUT);  // Define pin 3 as an input port, to accept digital input
   pinMode(Led13,OUTPUT);            // Define LED13 as an output port, to indicate digital trigger reached
+
+  // For the RGB
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+  digitalWrite(RED, HIGH);
+  digitalWrite(GREEN, LOW);
+  digitalWrite(BLUE, LOW);
 }
 
 void loop(){
+
+  
   analogValue = analogRead(sensorAnalogPin); // Read the value of the analog interface A0 assigned to digitalValue 
   digitalValue=digitalRead(sensorDigitalPin); // Read the value of the digital interface 12 assigned to digitalValue 
   Serial.println(analogValue); // Send the analog value to the serial transmit interface
+
+
+  if ( 565 < analogValue < 569) {
+    redValue = 0;
+    greenValue = 255;
+    analogWrite(RED, redValue);
+    analogWrite(GREEN, greenValue);
+  }
+
+  if ( 568 < analogValue < 572) {
+      redValue = 35;
+      greenValue = 210;
+      analogWrite(RED, redValue);
+      analogWrite(GREEN, greenValue);
+  }
+
+  if ( 571 < analogValue < 574) {
+      redValue = 70;
+      greenValue = 175;
+      analogWrite(RED, redValue);
+      analogWrite(GREEN, greenValue);
+  }
+
+  if ( 575 < analogValue < 578) {
+      redValue = 105;
+      greenValue = 140;
+      analogWrite(RED, redValue);
+      analogWrite(GREEN, greenValue);
+      delay(1000);
+  }
+
+  if ( 580 < analogValue < 584) {
+      redValue = 175;
+      greenValue = 70;
+      analogWrite(RED, redValue);
+      analogWrite(GREEN, greenValue);
+  }
+
+  if ( 583 < analogValue < 587) {
+      redValue = 210;
+      greenValue = 35;
+      analogWrite(RED, redValue);
+      analogWrite(GREEN, greenValue);
+      delay(1000);
+  }
+
+  if ( 586 < analogValue < 590) {
+      redValue = 255;
+      greenValue = 0;
+      analogWrite(RED, redValue);
+      analogWrite(GREEN, greenValue);
+  }
   
-  if(digitalValue==HIGH)      // When the Sound Sensor sends signla, via voltage present, light LED13 (L)
+  if(digitalValue==HIGH)      // When the Sound Sensor sends signals, via voltage present, light LED13 (L)
   {
     digitalWrite(Led13,HIGH);
+ 
   }
   else
   {
--- Week3/notes-week3.md
+++ Week3/notes-week3.md
@@ -2,6 +2,8 @@
 
 ## Task 1
 
+**Files**  sine_wave.ino, Sound_Sensor_Example_NR.ino, Task1.ino <br />
+
 Sometimes, the wavefrom produced from the sound sensor output wasn't perfect.
 
 <img src="squiggly-lines.jpg"
@@ -9,9 +11,10 @@ Sometimes, the wavefrom produced from the sound sensor output wasn't perfect.
      />
 
 The following helped get the perfect sound waveform using the sound sensor (i.e. no noise and detects speech not just sharp blows):
-	- Adjust screw on sound sensor until LED2 just goes out. 
-	- Attach the output wire to the correct pin.
-Ensure there is no background noise when calibrating.
+ - Adjust screw on sound sensor until LED2 just goes out. 
+ - Attach the output wire to the correct pin.
+ - Wait a minute or so before the sound sensor picks up sound to the right precision. 
+ - Ensure there is no background noise when calibrating.
 
 <img src="sound-sensor-waveform.jpg"
      alt="Waveform produced by sound sensor"
@@ -21,8 +24,22 @@ Ensure there is no background noise when calibrating.
 
 **Finding the minimum and maximum output voltages when I talk** <br />
 
-Talking into the microphone, the following plot was found. This means the range of my voice is between 566 and 586.
+Talking into the microphone, the following plot was found. This means the range of my voice is between 566 and 586. NB: RGB(red, green, blue)
+    566 = RGB(0, 255, 0)
+    569 = RGB(35, 210, 0)
+    572 = RGB(70, 175, 0)
+    575 = RGB(105, 140, 0)
+    578 = RGB(140, 105, 0)
+    581 = RGB(175, 70, 0)
+    584 = RGB(210, 35, 0)
+    587 = RGB(255, 0, 0)
 
 <img src="my-voice-range.jpg"
      alt="Finding my voice range"
      />
+
+**Implementing the RGB bulb** <br />
+Firstly, the bulb was setup using the same layout as last week. Each colour was tested to ensure it was working and the microphone was tested simultanously to checked the were working together. <br />
+
+A range of delay times were tested. Too short a delay time and the change in colour wouldn't be seen. Too long a delay time and the lag would be too great to see the effect. In the end, a comprimise was found, although 
+
